192c578aab3b18e02023182b481cea0380cf4a49
Paul MAILLARDET <polo@polosson.com>
2019-04-13 16:17:11 +0200

    Fix build & serve configuration

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
     outputDir: 'dist',
+    pages: {
+        demo: {
+            entry: 'public/demo.ts',
+            template: 'public/demo.html',
+            filename: 'index.html',
+        },
+    },
     configureWebpack: {
         output: {
             libraryExport: 'default',
